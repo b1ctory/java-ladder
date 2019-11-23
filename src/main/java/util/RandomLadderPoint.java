6be867fc3f77a0467411ed5c386a28536ace75cd
b1ctory <suebin710@gmail.com>
2019-11-23 18:01:41 +0900

    feat : LadderRule interface 생성

--- src/main/java/util/RandomLadderPoint.java
+++ src/main/java/util/RandomLadderPoint.java
@@ -2,7 +2,7 @@ package util;
 
 import java.util.Random;
 
-public class RandomLadderPoint {
+public class RandomLadderPoint implements LadderRule {
     public boolean isPoint() {
         Random random = new Random();
         return random.nextBoolean();
