03496348ae6b108eb518742ebfaa0930ee2fbcc5
b1ctory <suebin710@gmail.com>
2019-11-23 17:53:14 +0900

    feat : OutputView 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,25 +1,37 @@
 package view;
 
+import model.LadderGameResult;
 import model.ladder.Ladder;
 import model.ladder.LadderLine;
+import model.ladder.Point;
+import model.reward.RewardGroup;
 import model.user.UserGroup;
 
+import java.util.List;
+
 public class OutputView {
-    private static final String HORIZONTAL_LINE = "|";
-    private static final String VERTICAL_LINE = "-----";
-    private static final String EMPTY_LINE = "     ";
     private static final int NAME_CONTAINER = 6;
 
-    public static void printResult(Ladder ladder, UserGroup userGroup) {
-        System.out.println("\n실행 결과\n");
-        printName(userGroup);
+    public static void printLadderResult(Ladder ladder, UserGroup participantGroup, RewardGroup rewardGroup) {
+        System.out.println("\n사다리 결과\n");
+        printName(participantGroup);
         drawLadder(ladder);
+        printRewards(rewardGroup);
+        System.out.println();
     }
 
     private static void printName(UserGroup userGroup) {
         userGroup.getUserList().stream()
                 .forEach(x -> System.out.print(x.toString() + nameInterval(x.toString().length())));
         System.out.println();
+    }
+
+    public static String nameInterval(int nameLength) {
+        StringBuilder interval = new StringBuilder();
+        for (int i = 0; i < NAME_CONTAINER - nameLength; i++) {
+            interval.append(" ");
+        }
+        return interval.toString();
     }
 
     private static void drawLadder(Ladder ladder) {
@@ -30,33 +42,26 @@ public class OutputView {
     }
 
     private static void drawHorizontalLine(LadderLine line) {
-        for (Boolean b : line.getPoints()) {
-            System.out.print(HORIZONTAL_LINE);
-            lineOrEmpty(b);
+        for (Point p : line.getPoints()) {
+            System.out.print(line.toString() + p.toString());
         }
     }
 
-    private static void lineOrEmpty(boolean isPoint) {
-        if (isPoint) {
-            drawLine();
-            return;
-        }
-        drawEmpty();
+    private static void printRewards(RewardGroup rewardGroup) {
+        rewardGroup.getRewardList()
+                .forEach(reward -> System.out.print(reward.toString() + nameInterval((reward.toString().length()))));
+        System.out.println();
     }
 
-    private static void drawLine() {
-        System.out.print(VERTICAL_LINE);
+    public static void printGameResult(LadderGameResult ladderGameResult) {
+        List<String> names = InputView.inputResultName();
+        System.out.println("\n실행 결과");
+        ladderGameResult.getResult(names).entrySet()
+                    .forEach(entry -> printResult(entry.getKey(), entry.getValue()));
+        System.out.println();
     }
 
-    private static void drawEmpty() {
-        System.out.print(EMPTY_LINE);
-    }
-
-    public static String nameInterval(int nameLength) {
-        StringBuilder interval = new StringBuilder();
-        for (int i = 0; i < NAME_CONTAINER - nameLength; i++) {
-            interval.append(" ");
-        }
-        return interval.toString();
+    private static void printResult(String name, String reward) {
+        System.out.println(name + " : " + reward);
     }
 }
