ddc07a15fd5c6ef2545e1e8ae5ad84bcbe34620f
b1ctory <suebin710@gmail.com>
2019-11-23 18:02:14 +0900

    feat : LadderApplication 생성

--- src/main/java/LadderApplication.java
+++ src/main/java/LadderApplication.java
@@ -2,7 +2,6 @@ import controller.LadderController;
 
 public class LadderApplication {
     public static void main(String[] args) {
-        LadderController ladderController = new LadderController();
-        ladderController.gameResult();
+        new LadderController().gameResult();
     }
 }
